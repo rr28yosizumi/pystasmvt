1ac494a849d9889bb6edb75e6fec2cdd8f070daf
t_watanabe <t_watanabe@incrementp.co.jp>
2018-04-05 07:19:26 +0900

    first commit

---
+++ pystasmvt/mvtsql.py
@@ -0,0 +1,124 @@
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import io
+import sys
+import itertools
+
+
+def generate_queris(layers,scale_level):
+    queries = []
+    for layer in layers['Layer']:
+        if scale_level in layer['enable_scale']: 
+            queries.append(generate_sql(layer))
+    
+    if not queries:
+        return ''
+
+    queri =  " UNION ALL ".join(queries) + ";"
+    return queri
+
+def generate_sql(layer,bounds=4096,buffer=256,clip=True):
+    """ SQLの作成
+
+    """
+    geofunc = ''
+    if layer['geotype'] == 'line':
+        geofunc = 'ST_LineMerge(ST_Collect(geom))'
+    elif layer['geotype'] == 'polygon':
+        geofunc = 'ST_Union(geom)'
+    else:
+        geofunc = 'ST_Collect(geom)'
+    
+    sql = "SELECT ST_AsMVT(q, '{layername}', 4096, 'geom') "
+    sql += "FROM ("
+    sql += "    SELECT"
+    sql += "        {attr_col},"
+    sql += "        ST_AsMVTGeom("
+    sql += "            {geofunc},"
+    sql += "            st_makeenvelope(tile2lon({minx},{scale}), tile2lat({miny},{scale}), tile2lon({maxx},{scale}), tile2lat({maxy},{scale}), {srid}) ,"
+    sql += "            {bounds},"
+    sql += "            {buffer},"
+    sql += "            {clip}) AS geom"
+    sql += "    from ("
+    sql += "        SELECT {attr_col},(ST_Dump({geometry_col})).geom from {tablename} WHERE geom && st_makeenvelope(tile2lon({minx},{scale}), tile2lat({miny},{scale}), tile2lon({maxx},{scale}), tile2lat({maxy},{scale}), {srid}) "
+    sql += "    ) a GROUP BY {attr_col}"
+    sql += ") as q"
+    return sql.format(
+        **{'layername': layer['layername'],
+        'tablename': layer['tablename'],
+        'attr_col': layer['attr_col'],
+        'geometry_col': layer['geometry_col'],
+        'srid': layer['srid'],
+        'minx': '$2',
+        'miny' : '$3',
+        'maxx' : '$2+1',
+        'maxy' : '$3+1',
+        'scale' : '$1',
+        'geofunc': geofunc,
+        'bounds':bounds,
+        'buffer':buffer,
+        'clip':'true' if clip else 'false'
+        }
+    )
+
+def get_mvt(session,sql,zoom,x,y):
+    """ ベクタータイルのバイナリを生成
+
+    """
+    try:
+        response = list(session.execute(sql))
+        print(sql)
+    except:
+        # SQLに失敗した場合にロールバックしないとセッションをロックしてしまう。
+        session.rollback()
+        raise
+
+    layers = filter(None,list(itertools.chain.from_iterable(response)))
+    final_tile = b''
+    for layer in layers:
+        final_tile = final_tile + io.BytesIO(layer).getvalue() 
+    return final_tile
+
+class MvtSql(object):
+    def __init__(self,layers,scale_level,func_name):
+        self._scale_level=scale_level
+        self._func_name=func_name
+        result = generate_queris(layers,scale_level)
+        self._queri = result
+    
+    def get_query(self,x,y,z):
+        return self._queri.replace('$2+1',str(x+1)).replace('$3+1',str(y+1)).replace('$1',str(z)).replace('$2',str(x)).replace('$3',str(y))
+    
+    def get_prepare(self):
+        prepared = "PREPARE gettile_{0}(integer, integer, integer) AS ".format(self._func_name)
+        return prepared + self._queri
+
+    def get_execute(self,x,y,z):
+        execute = "EXECUTE gettile_{0}".format(self._func_name)
+        execute += "({0},{1},{2});"
+        return execute.format(x,y,z)
+    
+    def get_mvt_by_query(self,session,x,y,z):
+        try:								# Sanitize the inputs
+            sani_zoom,sani_x,sani_y = int(z),int(x),int(y)
+        except:
+            print('suspicious')
+            return 1
+
+        final_query = self.get_query(sani_x,sani_y,sani_zoom)
+        return get_mvt(session,final_query,z,x,y)
+    
+    def get_mvt_by_execute(self,session,x,y,z):
+        try:								# Sanitize the inputs
+            sani_zoom,sani_x,sani_y = int(z),int(x),int(y)
+        except:
+            print('suspicious')
+            return 1
+
+        final_query = self.get_execute(sani_x,sani_y,sani_zoom)
+        return get_mvt(session,final_query,z,x,y)
+
+    
+
+
---
+++ server.py
@@ -0,0 +1,100 @@
+import tornado.ioloop
+import tornado.web
+import io
+import os
+import json
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from pystasmvt import mvtsql
+
+# 設定ファイルの格納先
+LAYERCONFIG_PATH='./layerconfig.json'
+
+# 最大スケールレベル
+MAX_SCALE_LEVEL=19
+
+# EXECUTE文格納用
+_SCALE_SQL_LIST={}
+
+# セッションの格納先
+
+_ENGINE = None
+
+def get_layerconfig_from_json(file):
+    """ JSONファイルの読み込み
+    """
+    config = None
+    with open(file,'r') as stream:
+        config = json.load(stream)
+    return config
+    
+
+def init_db_session():
+    """ サーバ起動の事前準備処理
+
+    設定ファイルの読み込み
+    PREPAREの実行
+    EXECUTE文のリスト作成
+
+    """
+    layers = get_layerconfig_from_json(LAYERCONFIG_PATH)
+    if not layers:
+        return False
+
+    for scale in range(MAX_SCALE_LEVEL):
+        prepared = mvtsql.MvtSql(layers,scale,scale)
+        if prepared:
+            _SCALE_SQL_LIST[scale] = prepared
+    return True
+
+def get_mvt(zoom,x,y):
+    """ ベクタータイルのバイナリを生成
+
+    """
+    try:								# Sanitize the inputs
+        sani_zoom,sani_x,sani_y = int(zoom),int(x),int(y)
+        del zoom,x,y
+    except:
+        print('suspicious')
+        return 1
+
+    if sani_zoom not in _SCALE_SQL_LIST.keys():
+        return 1
+
+    DBSession = sessionmaker(bind=_ENGINE)
+    session = DBSession()
+    final_query = _SCALE_SQL_LIST[sani_zoom]
+    try:
+        return final_query.get_mvt_by_query(session,sani_x,sani_y,sani_zoom)
+    except:
+        # SQLに失敗した場合にロールバックしないとセッションをロックしてしまう。
+        session.rollback()
+        raise
+
+class GetTile(tornado.web.RequestHandler):
+    def get(self, zoom,x,y):
+        self.set_header("Content-Type", "application/x-protobuf")
+        self.set_header("Content-Disposition", "attachment")
+        self.set_header("Access-Control-Allow-Origin", "*")
+        response = get_mvt(zoom,x,y)
+        if response != 1:
+            self.write(response)
+
+def main():
+    if not init_db_session():
+        print('Failed initialize')
+        return
+    
+    application = tornado.web.Application([(r"/tiles/([0-9]+)/([0-9]+)/([0-9]+).pbf", GetTile)])
+    print("Postserve started..")
+    application.listen(8080)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    # コネクションの作成
+    _ENGINE = create_engine('postgresql://'+os.getenv('POSTGRES_USER','map')+':'+os.getenv('POSTGRES_PASSWORD','map')+'@'+os.getenv('POSTGRES_HOST','localhost')+':'+os.getenv('POSTGRES_PORT','5432')+'/'+os.getenv('POSTGRES_DB','gis_test2'),
+    pool_size=20, max_overflow=0)
+    
+    main()
